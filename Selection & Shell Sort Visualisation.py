579661f99004d3c33bb1d241155a2d2f696e8038
Fiky Anggara <71414823+fiky-web@users.noreply.github.com>
2022-08-10 00:32:01 +0700

    add algoritma selection sort

--- Selection & Shell Sort Visualisation.py
+++ Selection & Shell Sort Visualisation.py
@@ -11,3 +11,18 @@ root.config(bg='black')
 data1 = []
 data2 = []
 time_start = time.time()
+
+def selection_sort(data,drawArr,canvas):
+    global time_start
+    time_start = time.time()
+
+    for i in range(0, len(data)):
+        key = data[i]
+        j = i-1
+        while j >= 0 and key < data[j]:
+            data[j+1] = data[j]
+            drawArr(data, ['green' if x == j or x == j+1 else 'red' for x in range(len(data))], canvas)
+            updateTime(timer2,time_start)
+            j -= 1
+        data[j+1] = key
+    drawArr(data, ['green' for x in range(len(data))], canvas)
