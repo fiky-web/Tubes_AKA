a838591f7e87943268f41b2e0e9f1d86787b9773
Fiky Anggara <71414823+fiky-web@users.noreply.github.com>
2022-08-10 00:30:18 +0700

    Create Selection & Shell Sort Visualisation.py

---
+++ Selection & Shell Sort Visualisation.py
@@ -0,0 +1,9 @@
+from tkinter import *
+import random
+import time
+
+root = Tk()
+
+root.title('shell sort vs Selection sort by Fiky Anggara | 1303180015')
+root.maxsize(800,600)
+root.config(bg='black')
