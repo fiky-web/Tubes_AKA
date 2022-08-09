1fc8346fe1dd80ca64d395bc9a56c9545df64f75
Fiky Anggara <71414823+fiky-web@users.noreply.github.com>
2022-08-10 00:30:43 +0700

    Update Selection & Shell Sort Visualisation.py

--- Selection & Shell Sort Visualisation.py
+++ Selection & Shell Sort Visualisation.py
@@ -7,3 +7,7 @@ root = Tk()
 root.title('shell sort vs Selection sort by Fiky Anggara | 1303180015')
 root.maxsize(800,600)
 root.config(bg='black')
+
+data1 = []
+data2 = []
+time_start = time.time()
